41d95bdaff9a4ad33cb1348d6bf80af981c0c9ae
LonelyTomb <garubav@gmail.com>
2019-04-07 03:51:37 +0100

    Vuetify Setup Completed

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-toolbar fixed app>
+		<v-toolbar-side-icon @click="toggleDrawer" />
+		<v-toolbar-title v-text="title" />
+		<v-spacer />
+	</v-toolbar>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+	name: 'Header',
+	data: () => ({
+		title: 'Wuxia PWA'
+	}),
+	methods: {
+		...mapActions({
+			toggleDrawer: 'sidebar/toggleDrawer'
+		})
+	}
+};
+</script>
+
+<style scoped></style>
